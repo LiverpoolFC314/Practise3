9fa373082edd2b654648e63aa02bd86360d1f2dc
LiverpoolFC314 <gareshsingh794@gmail.com>
2026-03-12 17:41:48 +0000

    Add files via upload

---
+++ Practise3.py
@@ -0,0 +1,22 @@
+"""Let the length of the rectangle be 50 and height of the rectangle be 80. Calculate and print the perimeter and area of the rectangle"""
+height=80
+length=50
+perimeter= height*2+length*2
+area=80*50
+print("The",perimeter,"is and the",area, " is of  your rectangle ")
+
+
+
+"""Only children between the age of 10 and 15 (both inclusive) are allowed to take a ride at an amusement park. Write an expression for this check."""
+age=7
+if age>=10 and age<=15: 
+    print("You're allowed to take a ride in the amusement park")
+else: print("You're not allowed to take a ride in the amusement park")
+
+
+
+"""If each day of the week is represented with a number with Monday as 1, Tuesday as 2 and so on. Write an expression to check if it is a weekend. Get the day as input"""
+weeknumber=1
+if weeknumber>5: 
+    print("It a Weekend")
+else:print("It's a weekday ")
